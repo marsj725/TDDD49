b12bd35775dcc2a05f1b2bcd176747e654c7efb6
Samuel Nilsson <samni698@gmail.com>
2014-11-05 10:47:18 +0100

    Added a GUI class. This far it only creates an empty window.

---
+++ ChessGUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+public class ChessGUI : Form {
+
+	public ChessGUI() {
+		Text = "Chess";
+		Size = new Size(250, 200);
+		CenterToScreen();
+	}
+
+	static public void Main() {
+		Application.Run(new ChessGUI());
+	}
+
+}
+
