a1d871e65cdd7f0d8b404ada0fb6f650a8140dd2
Samuel Nilsson <samni698@gmail.com>
2014-11-07 12:25:41 +0000

    Added a class which extends textbox. The textbar is suppose to show all the draws made by the user and its opponent during a game session.

---
+++ GUI/ChessDrawDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+/// <summary>
+/// Displays the draws made by the user and the opponent.
+/// </summary>
+public class ChessDrawDisplay : TextBox {
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ChessDrawDisplay"/> class.
+	/// </summary>
+	public ChessDrawDisplay() {
+		this.ReadOnly = true;
+		this.Multiline = true;
+		this.ScrollBars = true;
+	}
+
+}
+
