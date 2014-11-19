0077681bca75922a0c6297bb59c6f848798eb0c6
Samuel Nilsson <samni698@gmail.com>
2014-11-19 12:56:42 +0100

    Added a new abstract class Player with unimplemented functions. Needed to create a new class GUIPlayer (since multiple inheritance isn't allowed in c#) which changes our class structure a little bit.

---
+++ Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/// <summary>
+/// A chess player.
+/// </summary>
+public abstract class Player {
+
+	private Piece.PieceColor color;
+
+	/// <summary>
+	/// Sets a value indicating whether it is the <see cref="Player"/>s turn in the game.
+	/// </summary>
+	/// <value><c>true</c> if my turn; otherwise, <c>false</c>.</value>
+	public bool MyTurn {
+		set { MyTurn = value; }
+	}
+
+	/// <summary>
+	/// Makes a draw.
+	/// </summary>
+	/// <returns><c>true</c>, if draw was made, <c>false</c> otherwise.</returns>
+	/// <param name="fromRow">From row.</param>
+	/// <param name="fromCol">From col.</param>
+	/// <param name="toRow">To row.</param>
+	/// <param name="toCol">To col.</param>
+	protected abstract bool makeDraw(int fromRow, int fromCol, int toRow, int toCol);
+
+	/// <summary>
+	/// Updates the board state.
+	/// </summary>
+	/// <param name="board">Board.</param>
+	public abstract void updateBoard(Piece[,] board);
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Player"/> class.
+	/// </summary>
+	/// <param name="color">Color.</param>
+	public Player(Piece.PieceColor color) {
+		MyTurn = false;
+		this.color = color;
+	}
+}
+
