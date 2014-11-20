4e182ec220c435023d4504190d2289f10df37347
Samuel Nilsson <samni698@gmail.com>
2014-11-20 12:26:23 +0100

    Fixed the compile error.

--- Engine/Board.cs
+++ Engine/Board.cs
@@ -16,23 +16,29 @@ public class Board {
 	/// Resets the board pieces to its initial positions.
 	/// </summary>
 	public void resetBoard() {
-		this.board[0, 0] = this.board[0, 7] = new Rook(Piece.PieceColor.BLACK);
-		this.board[0, 1] = this.board[0, 6] = new Knight(Piece.PieceColor.BLACK);
-		this.board[0, 2] = this.board[0, 5] = new Bishop(Piece.PieceColor.BLACK);
-		this.board[0, 3] = new Queen(Piece.PieceColor.BLACK);
-		this.board[0, 4] = new King(Piece.PieceColor.BLACK);
+		this.board[0, 0] = new Rook(Piece.PieceColor.BLACK, 0, 0);
+		this.board[0, 7] = new Rook(Piece.PieceColor.BLACK, 0, 7);
+		this.board[0, 1] = new Knight(Piece.PieceColor.BLACK, 0, 1);
+		this.board[0, 6] = new Knight(Piece.PieceColor.BLACK, 0, 6);
+		this.board[0, 2] = new Bishop(Piece.PieceColor.BLACK, 0, 2);
+		this.board[0, 5] = new Bishop(Piece.PieceColor.BLACK, 0, 5);
+		this.board[0, 3] = new Queen(Piece.PieceColor.BLACK, 0, 3);
+		this.board[0, 4] = new King(Piece.PieceColor.BLACK, 0, 4);
 
 		// Sets the positions of the pawns.
 		for(int i = 0; i < 8; i++) {
-			this.board[1, i] = new Pawn(Piece.PieceColor.BLACK);
-			this.board[6, i] = new Pawn(Piece.PieceColor.WHITE);
+			this.board[1, i] = new Pawn(Piece.PieceColor.BLACK, 1, i);
+			this.board[6, i] = new Pawn(Piece.PieceColor.WHITE, 1, i);
 		}
 
-		this.board[7, 0] = this.board[7, 7] = new Rook(Piece.PieceColor.BLACK);
-		this.board[7, 1] = this.board[7, 6] = new Knight(Piece.PieceColor.BLACK);
-		this.board[7, 2] = this.board[7, 5] = new Bishop(Piece.PieceColor.BLACK);
-		this.board[7, 3] = new Queen(Piece.PieceColor.BLACK);
-		this.board[7, 4] = new King(Piece.PieceColor.BLACK);
+		this.board[7, 0] = new Rook(Piece.PieceColor.WHITE, 7, 0);
+		this.board[7, 7] = new Rook(Piece.PieceColor.WHITE, 7, 7); 
+		this.board[7, 1] = new Knight(Piece.PieceColor.WHITE, 7, 1);
+		this.board[7, 6] = new Knight(Piece.PieceColor.WHITE, 7, 6); 
+		this.board[7, 2] = new Bishop(Piece.PieceColor.WHITE, 7, 5);
+		this.board[7, 5] = new Bishop(Piece.PieceColor.WHITE, 7, 5); 
+		this.board[7, 3] = new Queen(Piece.PieceColor.WHITE, 7, 3);
+		this.board[7, 4] = new King(Piece.PieceColor.WHITE, 7, 3);
 	}
 
 }
