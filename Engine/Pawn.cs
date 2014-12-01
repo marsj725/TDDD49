b67ac3cef9444fb60944892ef4c9ada75e9dfbaf
Samuel Nilsson <samni698@gmail.com>
2014-12-01 14:30:25 +0100

    Fixed the index bug in Pawn.

--- Engine/Pawn.cs
+++ Engine/Pawn.cs
@@ -1,12 +1,10 @@
 using System;
+
 public class Pawn : Piece {
 
-	private bool firstMove;
-
 	public Pawn(PieceColor color, int x, int y) : base(PieceType.PAWN, color, x, y) {
-		firstMove = true;
-	}
 
+	}
 
 	/// <summary>
 	/// Is the move legal for this piece.
@@ -17,18 +15,17 @@ public class Pawn : Piece {
 	/// <param name="fromColumn">From column.</param>
 	/// <param name="toRow">To row.</param>
 	/// <param name="toCol">To col.</param>
-	public override bool isMoveLegal (int fromRow, int fromCol, int toRow, int toCol)
-	{
+	public override bool isMoveLegal(int fromRow, int fromCol, int toRow, int toCol) {
 		//Vi måste implementera något som berättar vilken direction vi spelar
 		//Firstmove (Special)
-		if (fromRow == 7 || fromRow == 1) {
-			if (Math.Abs (fromRow - toRow) > 0 && Math.Abs (fromRow - toRow) <= 2) { 
+		if(fromRow == 6 || fromRow == 1) {
+			if(Math.Abs(fromRow - toRow) > 0 && Math.Abs(fromRow - toRow) <= 2) { 
 				return true;
 			} else {
 				return false;
 			}
-		//Hortizontal movement
-		} else if (Math.Abs (fromRow - toRow) == 1) {
+			//Hortizontal movement
+		} else if(Math.Abs(fromRow - toRow) == 1) {
 			return true;
 		} else {
 			return false;
